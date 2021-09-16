8cd814f2021f6b05da26fc253ed8f22c56a9720f
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-16 07:10:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+    The ShareX Server main file.
+*/
+const config = require('./config.json');
+let PORT = config.port || 3000;
+let connectURI = config.connectURI || "mongodb://localhost/sharex-server";
+
+const router = require('./routes');
+const colors = require('colors');
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+mongoose.connect(connectURI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const express = require('express');
+let app = express();
+app.set('trust proxy', true);
+app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/uploads/'));
+router.setup(app);
+app.get('/*', (req, res) => { return res.status(302).redirect('/404.html'); });
+
+app.listen(PORT, () => {
+    console.log('Starting ShareX Server on port: '.green + PORT.toString().white);
+});
+
+let discord = require('./discord/index');
+if (config.token) { discord.startBot(config.token, config.botOptions); }
+else console.log('Starting without a discord bot.'.red);
+
+if (!fs.existsSync('./uploads/')) fs.mkdirSync('./uploads/');
