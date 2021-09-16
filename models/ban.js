4058a0f6a7c740c84c6b165df2feb4c91fecfc2c
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-16 14:37:20 -0500

    Umm doesnt this look familiar :sob:

--- models/ban.js
+++ models/ban.js
@@ -1,10 +1,25 @@
 /*
-    The user model for mongoose
+LMAO
 */
 const mongoose = require("mongoose");
+
+const UrlSchema = mongoose.Schema({
+    banip: String
+});
+
+let BanModel = mongoose.model("banip", UrlSchema);
+
+let parseIP = async (ip) => {
+    ip = ip.replace('::ffff:', '').replace('::1', '127.0.0.1').replace('localhost', '127.0.0.1');
+    let ipTest = ip.split('.')[0] + '.' + ip.split('.')[ip.split('.').length - 1];
+    let ipData = await BanModel.findOne({ ip: ipTest });
+    if (ipData == null) return ip;
+    return 'undefined';
+};
 
 const BanSchema = mongoose.Schema({
     ip: String,
 });
 
-module.exports = mongoose.model("bans", BanSchema);
+module.exports = { parseIP, BanModel };
+module.exports = banModel = mongoose.model('banip', UrlSchema)
